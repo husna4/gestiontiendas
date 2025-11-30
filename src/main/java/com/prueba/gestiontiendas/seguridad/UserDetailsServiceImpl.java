ebb1935cd7a09f26bb2b128a62a63694adee1692
husnain <hchaudhry@gestion400.com>
2025-11-30 16:24:50 +0100

    Añadir funcionalidad JWT

---
+++ src/main/java/com/prueba/gestiontiendas/seguridad/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.prueba.gestiontiendas.seguridad;
+
+import com.prueba.gestiontiendas.modelo.Usuario;
+import com.prueba.gestiontiendas.repositorio.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @author Husnain
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles(usuario.getRole().name())
+                .build();
+    }
+}
